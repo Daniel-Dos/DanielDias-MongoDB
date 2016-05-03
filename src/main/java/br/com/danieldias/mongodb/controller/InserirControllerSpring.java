18e99f9ed793967888b6fbcae0e0790f6cdce671
Daniel-Dias <daniel.dias.analistati@gmail.com>
2016-05-03 18:57:49 -0300

    Incluindo com Spring

---
+++ src/main/java/br/com/danieldias/mongodb/controller/InserirControllerSpring.java
@@ -0,0 +1,43 @@
+package br.com.danieldias.mongodb.controller;
+
+import org.bson.Document;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import br.com.danieldias.mongodb.modelo.Pessoa;
+
+@Controller
+public class InserirControllerSpring {
+	
+	@RequestMapping("InserirSpring")
+	public String adiciona(Pessoa pessoa) {
+		
+		/*Criando a conexao com o MongoDB*/
+		MongoClient conexao = new MongoClient(); //host = localhost e porta = 27017
+		
+		//acessando ou criando uma database
+		MongoDatabase dataBase = conexao.getDatabase("OmegaWare"); // caso no exista, sera criado um database chamado OmegaWare
+		
+		//criando ou recuperando uma coleção 
+		//caso no exista, sera criado um Collection chamado Pessoas
+		MongoCollection<Document> collectionPessoas = dataBase.getCollection("Pessoas"); 
+		
+		// inserindo 
+		collectionPessoas.insertOne(
+				new Document("nome",pessoa.getNome())
+				.append("Idade", pessoa.getIdade())
+				.append("Profissão", pessoa.getProfissao())
+				.append("Endereço", pessoa.getEndereco())
+				.append("Cidade", pessoa.getCidade())
+				.append("Bairro", pessoa.getBairro())
+				.append("Estado", pessoa.getEstado()) );
+		
+		return "ok";
+	}
+	
+
+}
---
+++ src/main/webapp/InserirSpring.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Inserir no MongoDB</title>
+</head>
+<body>
+
+<form action="InserirSpring" method="post">
+
+   Nome: <input type="text" name="nome"/><br /><br />
+   Idade: <input type="number" name="idade"/><br /> <br />
+   Profissão: <input type="text" name="profissao"/><br /><br />
+   Endereço: <input type="text" name="endereco"/><br /><br />
+   Cidade: <input type="text" name="cidade"/><br /><br />
+   Bairro: <input type="text" name="bairro"/><br /><br />
+   Estado: <input type="text" name="estado"/><br /><br/>
+   
+   <input type="submit" value="Cadastrar Dados no MongoDB"/>
+
+
+</form>
+
+</body>
+</html>
